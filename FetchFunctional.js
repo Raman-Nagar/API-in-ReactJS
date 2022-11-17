aa3e744e8af292f4ef029cd591964869e9f8dc8c
Raman Nagar <115226318+Raman-Nagar@users.noreply.github.com>
2022-11-17 12:20:38 +0530

    Add files via upload

---
+++ FetchFunctional.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react'
+
+export default function FetchFunctional() {
+    let [data, setData] = useState([])
+    let [inp1, setInp1] = useState("")
+    let [inp2, setInp2] = useState("")
+    let [inp3, setInp3] = useState("")
+
+    function Getdata() {
+        fetch("http://localhost:3000/product.json")
+            .then((x) => x.json())
+            .then((res) => setData(res.products))
+            .catch(console.log)
+    }
+    useEffect(() => {
+        Getdata();
+    })
+    function Inp1(e) {
+        setInp1(e.target.value)
+        e.preventDefault();
+    }
+    function Inp2(e) {
+        setInp2(e.target.value)
+        e.preventDefault();
+    }
+    function Inp3(e) {
+        setInp3(e.target.value)
+        e.preventDefault();
+    }
+    function Formsubmit(e) {
+        fetch("http://localhost:4001/products", {
+            method: 'POST',
+            body: JSON.stringify({
+                "name": inp1,
+                "userid": inp2,
+                "address": inp3
+            }),
+            headers: { "content-type": "application/json; charset=UTF-8" }
+        })
+        e.preventDefault();
+    }
+    function ListUpdat(e) {
+        fetch(`http://localhost:4001/products/${e.target.value}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                "name": inp1,
+                "userid": inp2,
+                "address": inp3
+            }),
+            headers: { "content-type": "application/json; charset=UTF-8" }
+        })
+        e.preventDefault();
+    }
+    function ListDelet(e) {
+        fetch(`http://localhost:4001/products/${e.target.value}`, {
+            method: 'DELETE',
+            headers: { "content-type": "application/json; charset=UTF-8" }
+        })
+        e.preventDefault();
+    }
+    return (
+        <>
+            <div className='container my-5'>
+                <form className='row border border-dark rounded'>
+                    <input type="text" className="form-control col m-2" onChange={(e) => Inp1(e)} placeholder="enter your name" />
+                    <input type="text" className="form-control col m-2" onChange={(e) => Inp2(e)} placeholder="enter userID" />
+                    <input type="text" className="form-control col m-2" onChange={(e) => Inp3(e)} placeholder="enter address" />
+                    <button onClick={(e) => Formsubmit(e)} className="btn btn-primary">submit</button>
+                </form>
+                <table className='table table-striped border border-4 m-auto'>
+                    <tr className="border bg-info">
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Userid</th>
+                        <th>Address</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    </tr>
+                    <tbody>
+                        {data.map((x) => {
+                            return (
+                                <tr key={x.id} className="border">
+                                    <th>{x.id}</th>
+                                    <th>{x.name}</th>
+                                    <th>{x.userid}</th>
+                                    <th>{x.address}</th>
+                                    <td><button className='btn btn-success rounded' onClick={(e) => ListUpdat(e)} value={x.id}>Update</button></td>
+                                    <td><button className='btn btn-danger rounded' onClick={(e) => ListDelet(e)} value={x.id}>Delete</button></td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
+
+}
